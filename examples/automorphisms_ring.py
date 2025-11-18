9d0e2f77a03d93017e46490bc01e66ba589e3368
Sebastian <sgitt@dwavesys.com>
2025-11-17 18:57:21 -0800

    Add unit tests, clean up examples, and use class properties
    - Use class properties to calculate vertex and edge orbits on demand
    - Rename `test` method to `test_composability` and optimize mask search in sifting
    - Improve docstring formatting with consistent indentation
    - Add blank lines to enhance functions readability
    - Add release notes

--- examples/automorphisms_ring.py
+++ examples/automorphisms_ring.py
@@ -1,12 +1,30 @@
-import dwave_networkx as dnx
-import random
-import matplotlib.pyplot as plt
+# Copyright 2025 D-Wave
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+An example to demonstrate calculating the Schreier-Sims representation of a 
+cycle graph with 8 vertices, as well as the number of automorphisms,
+vertex orbits, and a random uniform sampling of 3 automorphisms.
+"""
+
 import networkx as nx
-import numpy as np
-from dwave.experimental.automorphism import *
+
+from dwave.experimental.automorphism import array_to_cycle, sample_automorphisms, schreier_rep
+
+
 G = nx.cycle_graph(8)
 
-result = schreier_rep(G, num_samples=4)
+result = schreier_rep(G)
 
 #print the elements of the group vector in cycle notation
 print('Schreier-Sims representation (not including identity):')
