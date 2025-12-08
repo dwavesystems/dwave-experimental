c4fed460351d8ef7489a306cc20b134b58e7c900
Jack Raymond <10591246+jackraymond@users.noreply.github.com>
2025-12-08 13:14:51 -0800

    Add missing space in string

--- dwave/experimental/multicolor_anneal/utils.py
+++ dwave/experimental/multicolor_anneal/utils.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Union, Optional
-
 import networkx as nx
 
 from dwave_networkx import zephyr_coordinates
